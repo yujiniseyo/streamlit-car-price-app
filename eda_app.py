57d43fee40a10b988376788169be53c1df53e96d
yujiniseyo <yujiniseyo@gmail.com>
2021-03-11 11:57:35 +0900

    자동차 구매 가격 예측 앱 ver.1.0

--- eda_app.py
+++ eda_app.py
@@ -29,4 +29,60 @@ def run_eda_app() :
     elif selected_radio == '통계치' :
         st.dataframe(car_df.describe())
 
+    columns = car_df.columns
+    columns = list(columns)
 
+    selected_cols = st.multiselect('컬럼을 선택하세요.' , columns)
+    if len(selected_cols) != 0 :
+        st.dataframe(car_df[selected_cols])
+    else :
+        st.write('선택한 컬럼이 없습니다.')
+    
+
+    # 상관계수를 화면에 보여주도록 만든다.
+    # 멀티셀렉트에 컬럼명 보여주고, 해당 컬럼들에 대한 상관계수를 보여준다.
+    # 단, 컬럼들은 숫자 컬럼들만 멀티 셀렉트에 나타나야 한다.
+
+    corr_cols = car_df.columns[car_df.dtypes != object]
+    selected_corr = st.multiselect('상관계수 컬럼을 선택하세요.' , corr_cols)
+
+    if len(selected_corr) != 0 :
+        st.dataframe(car_df[selected_corr].corr())
+        
+        # 위에서 선택한 컬럼들을 이용해서, sns.pairplot을 그린다.
+
+        fig = sns.pairplot(data = car_df[selected_corr])
+        st.pyplot(fig)
+
+    else :
+        st.write('선택한 컬럼이 없습니다.')
+
+    
+
+    # 컬럼을 하나만 선택하면, 해당 컬럼의 max 와 min에 해당하는 사람의 데이터를 화면에 보여준다. (수치 데이터만)
+    
+    min_max_cols = car_df.columns[car_df.dtypes == float]
+    selected_min_max = st.selectbox('Max/Min 값을 볼 컬럼을 선택하세요.', min_max_cols)
+
+    st.write('Max', car_df.loc[car_df[selected_min_max]==car_df[selected_min_max].max(), ])
+    st.write('Min', car_df.loc[car_df[selected_min_max]==car_df[selected_min_max].min(), ])
+
+
+    # 고객 이름을 검색하면, 해당 고객의 데이터를 화면에 보여준다.
+
+    word = st.text_input('고객의 이름을 입력하세요')
+    #                                                              대소문자 구분 없이!
+    result = car_df.loc[car_df['Customer Name'].str.contains(word, case = False)]
+    if len(word) != 0 :
+        st.write(word+'님의 데이터를 검색하셨습니다.')
+        st.dataframe(result)
+        
+
+
+
+
+
+    
+
+
+    
--- ml_app.py
+++ ml_app.py
@@ -20,22 +20,54 @@ import joblib
 def run_ml_app():
     st.subheader('Machine Learning')
 
+    # 1. 유저한테 입력을 받는다.
+    # 성별
+    gender = st.radio('성별을 선택하세요.', ['여자', '남자'])
+    if gender == '여자' :
+        gender = '0'
+    else :
+        gender = '1'
+
+    # 나이
+    age = st.number_input('나이를 입력하세요.', min_value = 0, max_value = 120)
+
+    # 연봉
+    salary = st.number_input('연봉을 입력하세요.', min_value = 0)
+
+    # 카드 빚
+    debt = st.number_input('카드 빚을 입력하세요.', min_value = 0)
+
+    # 자산
+    worth = st.number_input('자산을 입력하세요.', min_value = 0)
+
+    # 2. 예측한다.
+    # 2-1. 모델을 불러온다.
     model = tensorflow.keras.models.load_model('data/car_ai.h5')
 
-    new_data  = np.array([0,  38, 90000,2000,500000])
+    # 2-2. np.array를 만든다.
+    new_data  = np.array([gender, age, salary, debt, worth])
 
+    # 2-3. 피처 스케일링을 한다.
     new_data = new_data.reshape(1,-1)
 
     sc_X = joblib.load('data/sc_X.pkl')
 
     new_data = sc_X.transform(new_data)
+    # 2-4. 예측한다.
 
     y_pred = model.predict(new_data)
 
+    # 예측 결과는, 스케일링 된 결과이므로 다시 돌려야한다.
     st.write(y_pred[0][0])
 
     sc_y = joblib.load('data/sc_y.pkl')
 
     y_pred_original = sc_y.inverse_transform(y_pred)
 
-    st.write(y_pred_original)
+    # st.write(y_pred_original)
+
+    # 3. 결과를 화면에 보여준다.
+    bnt = st.button('결과 보기')
+    if bnt :
+        st.write('예측 결과입니다. {:,.1f}달러의 차를 살 수 있습니다.'.format(y_pred_original[0,0]))
+
